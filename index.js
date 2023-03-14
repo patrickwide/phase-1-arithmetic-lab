60b39a5078eb811d826cda574ddf7e2655035f70
patrickwide <patrickwide254@gmail.com>
2023-03-15 02:40:21 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// Set the values of num1, num2, num3, and num4
+const num1 = 31;
+const num2 = 2;
+const num3 = 10;
+const num4 = 6;
 
-// Write your code here
+// Multiply num1 and num2 to get 62
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Calculate the remainder of dividing num3 by num4; the remainder should be 4
+const mod = num3 % num4;
+
+// Find the highest number in a set; the value returned should be 20
+const max = Math.max(5, 10, 15, 20);
+
+console.log(multiply); // Output: 62
+console.log(random); // Output: Random number greater than 0
+console.log(mod); // Output: 1
+console.log(max); // Output: 20
